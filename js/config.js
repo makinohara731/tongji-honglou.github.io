c36c1da9225e7e64b4cfec8713946e3ebf1fc110
makinohara731 <makinohara20050410@gmail.com>
2025-12-10 22:34:38 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// 全局配置
+const SITE_CONFIG = {
+    name: "同济大学红楼梦研究社",
+    giscus: {
+        repo: "makinohara731/tongji-honglou.github.io",
+        repoId: "R_kgDOQmDBtg",
+        category: "General",
+        categoryId: "DIC_kwDOQmDBts4CznfX"
+    }
+};
